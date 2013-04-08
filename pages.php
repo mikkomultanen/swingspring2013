7e2aef71299afda34bf06788c6406cf8a120dae5
Mikko Multanen <mikko.multanen@gmail.com>
2013-04-08 22:26:47 +0300

    Spread the costs page and add more information to other pages

--- pages.php
+++ pages.php
@@ -1,11 +1,11 @@
 <?php
 $pages = array();
 $pages[] = array('news', 'Uutiset', 'News');
-$pages[] = array('teachers', 'Valmennus', 'Teachers');
+$pages[] = array('classes', 'Tunnit', 'Classes');
+$pages[] = array('teachers', 'Valmentajat', 'Teachers');
 $pages[] = array('parties', 'Iltatanssit', 'Parties');
 $pages[] = array('competitions', 'Kilpailut', 'Competitions');
-$pages[] = array('travel', 'Leiripaikka', 'Travel');
-$pages[] = array('costs', 'Hinnat', 'Costs');
+$pages[] = array('travel', 'Leiripaikka', 'Housing & Travel');
 $pages[] = array('register', 'Ilmoittaudu', 'Register');
 $pages[] = array('contact', 'Yhteystiedot', 'Contact');
 ?>
