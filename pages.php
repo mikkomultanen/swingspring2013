e5d5cbfe30bf74a22647029967abfe6e20bb763c
Mikko Multanen <mikko.multanen@gmail.com>
2013-04-09 21:24:50 +0300

    Add schedule page

--- pages.php
+++ pages.php
@@ -5,6 +5,7 @@ $pages[] = array('classes', 'Tunnit', 'Classes');
 $pages[] = array('teachers', 'Valmentajat', 'Teachers');
 $pages[] = array('parties', 'Iltatanssit', 'Parties');
 $pages[] = array('competitions', 'Kilpailut', 'Competitions');
+$pages[] = array('schedule', 'Aikataulu', 'Schedule');
 $pages[] = array('travel', 'Leiripaikka', 'Housing & Travel');
 $pages[] = array('register', 'Ilmoittaudu', 'Register');
 $pages[] = array('contact', 'Yhteystiedot', 'Contact');
